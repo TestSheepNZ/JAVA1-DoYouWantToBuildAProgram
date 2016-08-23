161f8ebed8d6f47dfc9f4b8b84ba5d71566f9c87
TestSheepNZ <mike_talks@hotmail.com>
2016-08-23 19:26:09 +1200

    Final version - added comments

--- HelloWorld/src/ayeUpWorld.java
+++ HelloWorld/src/ayeUpWorld.java
@@ -1,3 +1,13 @@
+/*
+ * So this is a Hello World program, not a huge deal, but it's part of a series that
+ * I'm running on learning Java - find out more here ...
+ * http://testsheepnz.blogspot.co.nz/2016/08/java-1-do-you-want-to-build-program.html
+ * 
+ * For more information - please reread.
+ * 
+ * Mike Talks, Aug 2016
+ */
+
 
 public class ayeUpWorld {
 
@@ -8,3 +18,4 @@ public class ayeUpWorld {
 	}
 
 }
+
